39446cb1417aa9dd0a62c214e3e59d00c9048d85
Samya-S <77977204+Samya-S@users.noreply.github.com>
2022-07-28 07:18:16 +0530

    Update chapterwise-solved-pyqs.php

--- materials/chapterwise-solved-pyqs.php
+++ materials/chapterwise-solved-pyqs.php
@@ -120,7 +120,7 @@
     <div style="padding: 2%; background-image: url('../images/tg-bg.jpg');">
       <h1>Arihant</h1>
       <h2>Chapter wise solved previous 20 years' JEE  Main papers</h2>    
-      <a href="https://t.me/jeechallenger/180" target="_blank"><button class="button2_tele" style="font-size: 20px">Get it now</button></a>
+      <a href="https://t.me/jeechallenger/372" target="_blank"><button class="button2_tele" style="font-size: 20px">Get it now</button></a>
     </div>
 
 
