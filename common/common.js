c226a2e39ab5ed959558b7c7213b40e26d17aae3
Samya-S <77977204+Samya-S@users.noreply.github.com>
2022-07-29 14:20:45 +0530

    Update common.js

--- common/common.js
+++ common/common.js
@@ -27,14 +27,14 @@ document.onkeydown = function(e) {
 
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-if (prevScrollpos > currentScrollPos) {
-	document.getElementById("navbar").style.top = "0";
-} 
-else {
-	document.getElementById("navbar").style.top = "-100px";
-}
-prevScrollpos = currentScrollPos;
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } 
+  else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
 }
 
 
