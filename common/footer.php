8f38128802036d3583bbdce2b14f55d8d0db4d54
Samya-S <77977204+Samya-S@users.noreply.github.com>
2022-08-03 15:13:15 +0530

    https://bit.ly/SamyaSahaYt

--- common/footer.php
+++ common/footer.php
@@ -14,7 +14,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <a href="https://www.facebook.com/SamyaSaha08" target="_blank" class="fa fa-facebook"></a>
         <a href="https://www.twitter.com/SamyaSaha08" target="_blank" class="fa fa-twitter"></a>
-        <a href="https://www.youtube.com/channel/UCDGNdFdN619aOTVkOht6YvA?sub_confirmation=1" target="_blank" class="fa fa-youtube"></a>
+        <a href="https://bit.ly/SamyaSahaYt" target="_blank" class="fa fa-youtube"></a>
         <a href="https://www.instagram.com/SamyaSaha08" target="_blank" class="fa fa-instagram"></a><br>
         Copyright &copy 2020-22 <b><i>Samya Saha</i></b><br>- All Rights Reserved.<br><br>
         <h6>Source code available on Github: <a href="https://github.com/Samya-S/jee-challenger" target="_blank" style="text-decoration: none; isolation: unset;">Click here</a></h6>
